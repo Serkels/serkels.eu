f70a654f9a011f797a9d145646d3815d185bd8ab
Douglas DUTEIL <douglasduteil@gmail.com>
2023-04-24 22:44:49 +0200

    chore: switch to pnpm@^8

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
 }
+
+export default MyDocument;
