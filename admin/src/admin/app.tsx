1ea3c973af9cca3e108bfbf01970c5cfeb7f04ca
Douglas DUTEIL <douglasduteil@gmail.com>
2023-05-06 04:02:30 +0200

    feat(admin): udpate login page

---
+++ admin/src/admin/app.tsx
@@ -0,0 +1,26 @@
+//
+
+// @ts-ignore
+import favicon from "./extensions/favicon.png";
+
+//
+
+export default {
+  config: {
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Toc-Toc ?",
+        "Auth.form.welcome.subtitle": "You're knocking at a locked door 👀",
+      },
+    },
+    auth: {
+      logo: favicon,
+    },
+    menu: {
+      logo: favicon,
+    },
+    head: {
+      favicon,
+    },
+  },
+};
