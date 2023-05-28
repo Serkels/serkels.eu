bf62070ad1878270083b4cd129017d166f0aa29f
Douglas DUTEIL <douglasduteil@gmail.com>
2023-05-28 16:02:50 +0200

    feat: add nextjs-toploader

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import type { PropsWithChildren } from "react";
 import "./globals.css";
 
@@ -10,12 +11,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Toc-Toc",
   description: "Réseau d'échanges étudiant",
+  icons: { icon: "/favicon.svg" },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextTopLoader />
+        {children}
+      </body>
     </html>
   );
 }
