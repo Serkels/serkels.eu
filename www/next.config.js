6ca829af18b2707cb7c59f2ce11272d5d0ec4159
Douglas DUTEIL <douglasduteil@gmail.com>
2023-05-28 16:06:16 +0200

    chore(www): ignore lint during build

--- www/next.config.js
+++ www/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   transpilePackages: ["@1/ui", "@1/tailwindcss-config"],
 };
