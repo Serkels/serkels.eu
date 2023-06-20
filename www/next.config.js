95f01a814f0625caff02b6141a27a0a22932bb6a
Douglas DUTEIL <douglasduteil@gmail.com>
2023-06-20 12:55:16 +0200

    chore(lock): update admin deps

--- www/next.config.js
+++ www/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // serverRuntimeConfig: {
-  //   STRAPI_API_URL: process.env.STRAPI_API_URL,
-  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
