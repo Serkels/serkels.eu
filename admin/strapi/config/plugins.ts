8a4fbf287a38acf04e834fa90a0074d55ae906f7
Douglas DUTEIL <douglasduteil@gmail.com>
2023-06-24 12:25:15 +0200

    feat(admin): add @strapi/provider-upload-aws-s3 (2)

--- admin/strapi/config/plugins.ts
+++ admin/strapi/config/plugins.ts
@@ -39,6 +39,25 @@ module.exports = ({ env }) => ({
       sendMetadata: true,
     },
   },
+
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   // passwordless: {
   //   enabled: true,
   //   config: {
