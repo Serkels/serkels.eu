9edfb0ba465bc65db49184398ce1106cbf96e4ce
Douglas DUTEIL <douglasduteil@gmail.com>
2023-06-29 19:56:24 +0200

    feat(www): use common filter context

--- www/src/app/opportunity/[slug]/page.tsx
+++ www/src/app/opportunity/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { Grid } from "@1/ui/components/Grid";
 import { InputSearch } from "@1/ui/components/InputSearch";
 import type { _24_HOURS_ } from "@douglasduteil/datatypes...hours-to-seconds";
 import { OpportunityCategories } from "../OpportunityCategories";
+import { OpportunityFilterContextProvider } from "../OpportunityFilter.context";
 import { OpportunityArticle } from "./OpportunityArticle";
 import { SeeAlso } from "./SeeAlso";
 
@@ -21,16 +22,18 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <Grid className="px-0">
-      <Aside title="Opportunités">
-        <InputSearch />
-        <OpportunityCategories />
-      </Aside>
-      <main className="col-span-full bg-white md:col-span-6 xl:col-span-9">
-        <OpportunityArticle slug={slug} initialData={data} />
-      </main>
-      <aside className="col-span-3 hidden lg:px-10">
-        <SeeAlso category={category ?? "autres"} />
-      </aside>
+      <OpportunityFilterContextProvider>
+        <Aside title="Opportunités">
+          <InputSearch />
+          <OpportunityCategories />
+        </Aside>
+        <main className="col-span-full bg-white md:col-span-6 xl:col-span-9">
+          <OpportunityArticle slug={slug} initialData={data} />
+        </main>
+        <aside className="col-span-3 hidden lg:px-10">
+          <SeeAlso category={category ?? "autres"} />
+        </aside>
+      </OpportunityFilterContextProvider>
     </Grid>
   );
 }
