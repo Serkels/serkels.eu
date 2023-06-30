55f2cf3095afdcaf7e06953027a92a914e58d3ad
Douglas DUTEIL <douglasduteil@gmail.com>
2023-06-30 17:32:40 +0200

    feat(www): add global loader

---
+++ www/src/app/loading.tsx
@@ -0,0 +1,11 @@
+//
+
+import { Spinner } from "@1/ui/components/Spinner";
+
+export default function Loading() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <Spinner />
+    </main>
+  );
+}
