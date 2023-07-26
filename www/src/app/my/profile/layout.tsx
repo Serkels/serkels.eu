f17f73b73b8151066c6118b07f21e2b4d67247c5
Douglas DUTEIL <douglasduteil@gmail.com>
2023-07-26 04:00:02 +0200

    feat(www): responsive profile

--- www/src/app/my/profile/layout.tsx
+++ www/src/app/my/profile/layout.tsx
@@ -6,11 +6,10 @@ import { ProfileNavBar } from "./ProfileNavBar";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="col-span-full my-10 md:col-span-6 xl:col-span-9">
+    <div className="col-span-full my-10 px-4 md:col-span-6 md:px-0 xl:col-span-7">
       <AvatarMediaHorizontal />
 
       <ProfileNavBar className="my-10" />
-
       <hr className="my-8 border-none"></hr>
 
       {children}
