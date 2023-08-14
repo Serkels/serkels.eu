071a36a188d8a89de6ce0567f259223b844abe31
Douglas DUTEIL <douglasduteil@gmail.com>
2023-08-14 13:37:28 +0200

    fix(www): empty user profile

--- www/src/app/my/profile/history/History.tsx
+++ www/src/app/my/profile/history/History.tsx
@@ -1,27 +1,11 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-
 //
 
 export function History() {
-  const { data: session } = useSession();
-  if (!session) return null;
-
-  const data: any[] = [];
-  const exchanges = data.map((i) => <Item id={i} />);
-
-  //
-
-  if (!exchanges.length) {
-    exchanges.push(<Empty />);
-  }
-
   return (
     <ul>
-      {exchanges.map((ItemLike, i) => (
-        <li key={i}>{ItemLike}</li>
-      ))}
+      <Empty />
     </ul>
   );
 }
@@ -30,14 +14,6 @@ function Empty() {
   return (
     <li>
       <p className="text-center">N/A History</p>
-    </li>
-  );
-}
-
-function Item({ id }: { id: number }) {
-  return (
-    <li>
-      <p className="text-center">{id}</p>
     </li>
   );
 }
