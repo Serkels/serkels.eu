d78e09d628685d60bb1d06efde803e33bdc23301
Douglas DUTEIL <douglasduteil@gmail.com>
2023-08-24 00:59:38 +0200

    feat(exchanges): remove old view model (2)

--- admin/xxx/modules/profile/domain/index.ts
+++ admin/xxx/modules/profile/domain/index.ts
@@ -1,6 +1,6 @@
 //
 
-import { Entity, Ok, Result } from "@1/core/domain";
+import { Entity, Ok, Result, type ErrorInstance } from "@1/core/domain";
 import { z } from "zod";
 
 //
@@ -18,7 +18,7 @@ export const Profile_PropsSchema = z.object({
 export type Profile_Props = z.TypeOf<typeof Profile_PropsSchema>;
 
 export class Profile extends Entity<Profile_Props> {
-  static override create(props: Profile_Props): Result<Profile, Error> {
+  static override create(props: Profile_Props): Result<Profile, ErrorInstance> {
     return Ok(new Profile(props));
   }
 
