8889fe3caaed63388e9243c9db6c1e2ef5aef42d
Douglas DUTEIL <douglasduteil@gmail.com>
2023-08-27 14:15:53 +0200

    feat(www): add sentry (3)

--- www/src/app/my/exchange/[exchange_id]/deals/[deal_id]/layout.tsx
+++ www/src/app/my/exchange/[exchange_id]/deals/[deal_id]/layout.tsx
@@ -4,12 +4,12 @@ import type { PropsWithChildren } from "react";
 
 export default function Layout({
   children,
-  params,
-}: PropsWithChildren<{
+} // params,
+: PropsWithChildren<{
   params: { exchange_id: string; deal_id: string };
 }>) {
-  const exchange_id = Number(params.exchange_id);
-  const deal_id = Number(params.deal_id);
+  // const exchange_id = Number(params.exchange_id);
+  // const deal_id = Number(params.deal_id);
 
   return <>{children}</>;
 }
