dec04ff50336cada9c86268c8ab6dc8c8886ce0a
Douglas DUTEIL <douglasduteil@gmail.com>
2023-08-30 14:05:42 +0200

    save: broken transformations (2)

--- @1/modules/inbox/infra/strapi/Inbox_Schema_ToDomain.ts
+++ @1/modules/inbox/infra/strapi/Inbox_Schema_ToDomain.ts
@@ -6,7 +6,8 @@ import {
   type IAdapter,
   type IResult,
 } from "@1/core/domain";
-import { Inbox } from "../../domain";
+import { Profile } from "../../../profile/domain";
+import { Inbox, Thread } from "../../domain";
 import type { InboxList_Schema, Inbox_Schema } from "./Inbox_Schema";
 import { Thread_Schema_ToDomain } from "./Thread_Schema_ToDomain";
 
@@ -21,8 +22,23 @@ function reuslt_all_ok<A = any, B = any, M = any>(
 export class Inbox_Schema_ToDomain implements IAdapter<Inbox_Schema, Inbox> {
   thread_to_domain = new Thread_Schema_ToDomain();
   build(target: Inbox_Schema): IResult<Inbox, Error> {
-    return Inbox.create({ id: Number(target.id), thread: { id: 0 } as any });
+    const thread = Thread.create({
+      id: 0,
+      updated_at: new Date(),
+      profile: Profile.create({} as any).value(),
+      last_message: {},
+    } as any);
+
+    const all_results = Result.combine([thread]);
+    if (all_results.isFail()) {
+      return Result.fail(
+        new InputError("Inbox SubDomain", { cause: all_results.error() }),
+      );
+    }
+
+    return Inbox.create({ id: Number(target.id), thread: thread.value() });
   }
+
   build_list(list: InboxList_Schema): IResult<Inbox[], Error> {
     const results = list.map((inbox) =>
       this.build({ id: inbox.id, ...inbox.attributes }),
