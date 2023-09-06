f32d0dfbc9363b5b2d4c2890e564a41255589146
Douglas DUTEIL <douglasduteil@gmail.com>
2023-09-06 09:42:39 +0200

    fix(www): remove unused variables

--- www/src/app/(main)/door/[code]/(public)/(about)/About.tsx
+++ www/src/app/(main)/door/[code]/(public)/(about)/About.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import { useProfile } from "../layout.client";
 
 //
@@ -8,6 +7,5 @@ import { useProfile } from "../layout.client";
 export function About() {
   const profile = useProfile();
 
-  const session_context = useSession();
   return <p>{profile.get("about")}</p>;
 }
