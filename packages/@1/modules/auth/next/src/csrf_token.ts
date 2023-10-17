eb1cfbf318045f17ae601dfdf6a2d7754784061f
Douglas DUTEIL <douglasduteil@gmail.com>
2023-10-17 10:46:24 +0200

    feat(nextauth...trpc.prism): get cookie token name from auth options

--- packages/@1/modules/auth/next/src/csrf_token.ts
+++ packages/@1/modules/auth/next/src/csrf_token.ts
@@ -1,13 +1,3 @@
 //
 
-import { cookies } from "next/headers";
-
-//
-
-export function get_csrf_token() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("next-auth.csrf-token");
-  const csrfToken = token?.value.split("|")[0];
-
-  return csrfToken;
-}
+export { get_csrf_token } from "@douglasduteil/nextauth...trpc.prisma/next/csrf_token";
--- packages/@douglasduteil/nextauth...trpc.prisma/src/next/csrf_token.ts
+++ packages/@douglasduteil/nextauth...trpc.prisma/src/next/csrf_token.ts
@@ -1,12 +1,26 @@
 //
 
-import { cookies } from "next/headers";
+import type { AuthOptions } from "next-auth";
+import {
+  cookies as next_header_cookies,
+  headers as next_headers,
+} from "next/headers";
+import { defaultCookies } from "./cookie";
 
 //
 
-export function get_csrf_token() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("next-auth.csrf-token");
+export function get_csrf_token(authOptions = {} as AuthOptions) {
+  const headers = next_headers();
+  const cookieStore = next_header_cookies();
+  const url = new URL(headers.get("referer") || "http://localhost");
+  const next_auth_cookies = {
+    ...defaultCookies(
+      authOptions.useSecureCookies ?? url.protocol.startsWith("https://"),
+    ),
+    // Allow user cookie options to override any cookie settings above
+    ...(authOptions.cookies ?? {}),
+  };
+  const token = cookieStore.get(next_auth_cookies.csrfToken.name);
   const csrfToken = token?.value.split("|")[0];
 
   return csrfToken;
