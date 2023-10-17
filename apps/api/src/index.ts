7f9af09f05a5491d53feb9be64672a56adab606d
Douglas DUTEIL <douglasduteil@gmail.com>
2023-10-17 09:26:04 +0200

    feat(api): add sentry through @hono/sentry

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -9,6 +9,7 @@ import { Email_Sender } from "@1.infra/email";
 import { router } from "@1.infra/trpc";
 import type { Context } from "@1.module/trpc";
 import { serve } from "@hono/node-server";
+import { sentry } from "@hono/sentry";
 import { trpcServer } from "@hono/trpc-server";
 import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import {
@@ -27,14 +28,16 @@ import { z } from "zod";
 
 export const ENV = z
   .object({
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    //
+    EMAIL_FROM: z.string().email().default("no-reply@toc-toc.org"),
+    SENTRY_DSN: z.string().url(),
     SMTP_HOST: z.string(),
     SMTP_PASSWORD: z.string(),
     SMTP_PORT: z.coerce.number().optional(),
     SMTP_USERNAME: z.string(),
-    EMAIL_FROM: z.string().email().default("no-reply@toc-toc.org"),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
   })
   .parse(process.env);
 
@@ -60,7 +63,12 @@ function createContext(
 
 const app = new Hono();
 
-app.all("*", logger(), cors());
+app.all(
+  "*",
+  sentry({ dsn: ENV.SENTRY_DSN, debug: ENV.NODE_ENV === "development" }),
+  logger(),
+  cors(),
+);
 
 app.get("/", (c) =>
   c.json({
