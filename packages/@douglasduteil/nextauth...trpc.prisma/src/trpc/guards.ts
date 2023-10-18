074aac4d95662b9b8b1e482fe497d4fcc72cd905
Douglas DUTEIL <douglasduteil@gmail.com>
2023-10-18 21:48:49 +0200

    fix(nextauth...trpc.prisma): throw on no payload

--- packages/@douglasduteil/nextauth...trpc.prisma/src/trpc/guards.ts
+++ packages/@douglasduteil/nextauth...trpc.prisma/src/trpc/guards.ts
@@ -8,11 +8,22 @@ import { middleware } from "./trpc";
 
 export function verify_next_auth_token(secret: string) {
   return middleware(async ({ ctx, next }) => {
-    const payload = await decode({ token: ctx.headers.NEXTAUTH_TOKEN, secret });
+    try {
+      const payload = await decode({
+        token: ctx.headers.NEXTAUTH_TOKEN,
+        secret,
+      });
 
-    if (payload) {
-      return next({ ctx: { ...ctx, payload } });
+      if (payload) {
+        return next({ ctx: { ...ctx, payload } });
+      }
+
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        cause: new Error("No payload"),
+      });
+    } catch (cause) {
+      throw new TRPCError({ code: "PARSE_ERROR", cause });
     }
-    throw new TRPCError({ code: "UNAUTHORIZED" });
   });
 }
