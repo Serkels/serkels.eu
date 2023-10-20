30a21ab55c28cedd3d6b45b8aee03a4dcb7796cf
Douglas DUTEIL <douglasduteil@gmail.com>
2023-10-20 07:52:14 +0200

    debug: first infinit list with date cursor

--- apps/www/app/(main)/exchange/_client/List.tsx
+++ apps/www/app/(main)/exchange/_client/List.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { TRPC_React } from ":trpc/client";
 import type { Exchange } from "@1.modules/exchange.domain";
 import {
   Exchange_AsyncCard,
@@ -7,25 +8,11 @@ import {
 } from "@1.modules/exchange.ui";
 import { Card } from "@1.modules/exchange.ui/Card";
 import { card } from "@1.ui/react/card/atom";
+import { ErrorOccur } from "@1.ui/react/error";
 import type {
-  InfiniteQueryObserverSuccessResult,
   QueryObserverSuccessResult,
+  UseInfiniteQueryResult,
 } from "@tanstack/react-query";
-import { parse } from "date-fns";
-
-//
-
-const mock = [
-  {
-    id: "0000",
-    title: "Visite expo Picasso en couleurs",
-    available_places: 3,
-    places: 5,
-    when: parse("02/09/2023", "P", new Date()),
-    description:
-      "Dans une licence LEA, vous étudiez deux laus vous inscrivez à une licence où vous apprenez deux langues autres que l’anglais, vous aurez des cours d’anglais en plus). Le but n’est pas seulement de perfectionner communication…",
-  },
-] as Exchange[];
 
 //
 
@@ -33,24 +20,24 @@ export default function List() {
   // const { query, setQuery } = useSyncSearchQuery("q");
   // const info = TRPC_React.exchange.find.useInfiniteQuery(
   //   {},
-  // ) as UseInfiniteQueryResult<Exchange>;
-  const info = {
-    data: { pages: mock, pageParams: {} },
-    error: null,
-    isError: false,
-    isLoading: false,
-    isLoadingError: false,
-    isRefetchError: false,
-    isSuccess: true,
-    status: "success",
-  } as InfiniteQueryObserverSuccessResult<Exchange, unknown>;
+  // ) as UseInfiniteQueryResult<Exchange>;*
+  try {
+    const info = TRPC_React.exchange.find.useInfiniteQuery(
+      {},
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    ) as UseInfiniteQueryResult<Exchange>;
 
-  return (
-    <Exchange_InfiniteList info={info}>
-      {({ id }) => <Item key={id} id={id} />}
-    </Exchange_InfiniteList>
-    // <InputSearch {...field} {...props} />;
-  );
+    return (
+      <Exchange_InfiniteList info={info}>
+        {({ id }) => <Item key={id} id={id} />}
+      </Exchange_InfiniteList>
+      // <InputSearch {...field} {...props} />;
+    );
+  } catch (error) {
+    return <ErrorOccur error={error as Error} />;
+  }
 }
 
 function Item({ id }: { id: string }) {
@@ -60,7 +47,7 @@ function Item({ id }: { id: string }) {
   //   {},
   // ) as UseInfiniteQueryResult<Exchange>;
   const info = {
-    data: mock[0],
+    data: {},
     error: null,
     isError: false,
     isLoading: false,
--- packages/@1/modules/exchange/api/src/index.ts
+++ packages/@1/modules/exchange/api/src/index.ts
@@ -2,7 +2,6 @@
 
 import { Exchange_Schema, type Exchange } from "@1.modules/exchange.domain";
 import { next_auth_procedure, procedure, router } from "@1.modules/trpc";
-import { parse } from "date-fns";
 import { z } from "zod";
 
 //
@@ -17,18 +16,24 @@ const exchange_api_router = router({
     }),
 
   find: procedure
-    .input(z.object({ cursor: z.string().nullable() }))
-    .query(async () => {
-      return [
-        {
-          title: "Visite expo Picasso en couleurs",
-          available_places: 3,
-          places: 5,
-          when: parse("02/09/2023", "P", new Date()),
-          description:
-            "Dans une licence LEA, vous étudiez deux laus vous inscrivez à une licence où vous apprenez deux langues autres que l’anglais, vous aurez des cours d’anglais en plus). Le but n’est pas seulement de perfectionner communication…",
-        },
-      ] as Exchange[];
+    .input(
+      z.object({
+        cursor: z.date().default(new Date()),
+        limit: z.number().min(1).max(10).default(10),
+      }),
+    )
+    .query(async ({ input: { cursor, limit }, ctx: { prisma } }) => {
+      const items = await prisma.exchange.findMany({
+        cursor: { created_at: cursor },
+        orderBy: { created_at: "asc" },
+        take: limit + 1,
+      });
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (items.length > limit) {
+        const nextItem = items.pop();
+        nextCursor = nextItem!.created_at;
+      }
+      return { items, nextCursor };
     }),
 });
 
