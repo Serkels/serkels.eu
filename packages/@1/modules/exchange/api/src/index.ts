b8294bed8dd697b8583987320c7e871c2e41ba27
Douglas DUTEIL <douglasduteil@gmail.com>
2023-10-20 08:45:39 +0200

    feat(exchange): pull data from first date cursor

--- packages/@1/modules/exchange/api/src/index.ts
+++ packages/@1/modules/exchange/api/src/index.ts
@@ -18,22 +18,27 @@ const exchange_api_router = router({
   find: procedure
     .input(
       z.object({
-        cursor: z.date().default(new Date()),
+        cursor: z.date().optional(),
         limit: z.number().min(1).max(10).default(10),
       }),
     )
     .query(async ({ input: { cursor, limit }, ctx: { prisma } }) => {
+      const orderBy: NonNullable<
+        Parameters<typeof prisma.exchange.findFirst>[0]
+      >["orderBy"] = { created_at: "asc" };
       const items = await prisma.exchange.findMany({
-        cursor: { created_at: cursor },
-        orderBy: { created_at: "asc" },
+        ...(cursor ? { cursor: { created_at: cursor } } : {}),
+        orderBy,
         take: limit + 1,
       });
+
       let nextCursor: typeof cursor | undefined = undefined;
       if (items.length > limit) {
         const nextItem = items.pop();
         nextCursor = nextItem!.created_at;
       }
-      return { items, nextCursor };
+
+      return { data: items, nextCursor };
     }),
 });
 
