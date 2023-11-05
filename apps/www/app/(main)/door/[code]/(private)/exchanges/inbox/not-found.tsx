566f543e90358a7bc3a1743666737bf21df7f016
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-05 13:22:34 +0100

    save(exchange): actions (3)

---
+++ apps/www/app/(main)/door/[code]/(private)/exchanges/inbox/not-found.tsx
@@ -0,0 +1,11 @@
+//
+
+export default function Page_NotFound() {
+  return (
+    <main className="h-full bg-white">
+      <article className="px-4 pt-40 lg:px-16">
+        <h1 className="text-4xl font-bold">Exchange introuvable.</h1>
+      </article>
+    </main>
+  );
+}
