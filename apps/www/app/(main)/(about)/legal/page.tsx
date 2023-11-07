916d9ea33f946814e3dd270c2e0b03b279749e18
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-07 19:38:07 +0100

    feat(www): add lorem to about pages

--- apps/www/app/(main)/(about)/legal/page.tsx
+++ apps/www/app/(main)/(about)/legal/page.tsx
@@ -9,12 +9,23 @@ const ReactMarkdown = dynamic<any>(() => import("react-markdown"));
 const content = `
 # Politique d'utilisation
 
+> Conicio comis magnam tristis argentum aduro. Solium coepi argumentum virtus nulla amita approbo volubilis canto. Admoveo dolorum color bibo abduco sint avaritia aro.
+
+Argentum ceno carbo ustilo stabilis decet avaritia desipio. Subnecto certus veritatis et tracto benigne capitulus vestrum cariosus vindico. Vallum triumphus demum.
+Maxime viriliter canonicus clibanus sed corpus volutabrum tumultus aliqua cohibeo. Apostolus aureus vilis aliquam appositus teres talio strenuus vehemens. Defendo calcar spes cultellus excepturi via cubitum defendo adinventitias.
+Causa chirographum temperantia caelum. Valeo amplitudo stillicidium cognatus denuo turbo comitatus vilicus tui defungo. Stipes valetudo asperiores arcus coepi nobis antiquus vos aetas.
+Statua carcer caritas demulceo creta. Debitis venia qui. Adulescens demo natus adopto arcus abscido collum.
+Vos talio caecus. Minus appono votum provident adstringo universe custodia cognatus. Vaco calculus viduo utor trans crur vociferor calcar patrocinor.
+Derideo virtus caelum basium tamdiu decipio voluntarius. Tego aegre denuo circumvenio. Delinquo appositus approbo solio adimpleo arca ullam vitium creator.
+Claustrum demum tumultus suscipio tracto adeptio demitto succedo. Adfectus auctus canonicus quos arx valetudo derideo esse theca sponte. Tamen dolore terminatio.
+Tui curiositas perspiciatis cuius infit. Contigo somnus vulgo tergum custodia temeritas sulum. Curiositas paens utrimque nemo deorsum capillus corona curso.
+Arguo theca cena umbra veritatis. Solitudo triduana crebro virga calamitas cruciamentum spes tergo. Aliquam paulatim alienus cum utrum demoror.'
 
 `;
 
 export default function Page() {
   return (
-    <main className="prose lg:prose-xl container mx-auto my-32">
+    <main className="container prose mx-auto my-32 lg:prose-xl">
       <ReactMarkdown>{content}</ReactMarkdown>
     </main>
   );
--- apps/www/app/(main)/(about)/who/page.tsx
+++ apps/www/app/(main)/(about)/who/page.tsx
@@ -9,12 +9,22 @@ const ReactMarkdown = dynamic<any>(() => import("react-markdown"));
 const content = `
 # Qui sommes-nous ?
 
+> Nemo tepidus aestas. Cubicularis suscipit delicate ducimus tantum ipsam taceo nisi stipes talio. Amicitia conculco damno confero suus.
 
+Viriliter possimus sequi cupio nesciunt cauda theca aduro. Crur vigilo deorsum concedo coma umbra auctus uxor vaco. Appositus mollitia verus textus substantia alias.
+Textus aestus volaticus dolorum curis amicitia claustrum occaecati possimus. Demonstro accusamus cornu tamdiu bos. Angustus decumbo curo nulla solio minima crapula ulterius suffragium.
+Congregatio arcus delicate recusandae conicio arcesso caelestis. Spoliatio currus conor solutio solio conitor denego. Utroque verecundia taceo condico ars ambulo copiose.
+Fuga argentum maxime accusantium verto venia. Acceptus nihil magnam contigo summopere. Desipio venustas tam cohors terra cavus libero argumentum necessitatibus officiis.
+Depono conturbo incidunt canto accusator. Adamo coniecto appono tredecim aliqua velut terga auctus careo dolorem. Summa solvo vitae sint vigilo.
+Alioqui beatae avaritia modi. Copia tripudio cuius. Amissio bonus audeo comminor ea apparatus vivo caritas.
+Cernuus communis defungo tricesimus currus. Vilitas amaritudo tutamen nulla vinculum tollo. Autus validus defluo verbum sunt.
+Cur deprecator arbustum bellicus thermae consequuntur. Abduco asperiores voluntarius defessus caelestis porro acervus adamo cenaculum conforto. Aveho qui deludo absconditus suspendo antiquus.
+Celer comparo modi creator quas. Accusamus fugit demonstro illo aeneus aqua. Cogito vehemens summisse ciminatio stipes amet assentator supellex.
 `;
 
 export default function Page() {
   return (
-    <main className="prose lg:prose-xl container mx-auto my-32">
+    <main className="container prose mx-auto my-32 lg:prose-xl">
       <ReactMarkdown>{content}</ReactMarkdown>
     </main>
   );
