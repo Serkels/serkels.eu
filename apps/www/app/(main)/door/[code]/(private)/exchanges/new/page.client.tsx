d3fd43eadfc7d05f1727a9c1202ac8d49ed010de
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-08 02:34:39 +0100

    feat(www): redirect to exchange after creation

--- apps/www/app/(main)/door/[code]/(private)/exchanges/new/page.client.tsx
+++ apps/www/app/(main)/door/[code]/(private)/exchanges/new/page.client.tsx
@@ -8,12 +8,14 @@ import {
 } from "@1.modules/exchange.domain";
 import { Exchange_CreateForm } from "@1.modules/exchange.ui/form/new_exchange";
 import { Formik } from "formik";
+import { useRouter } from "next/navigation";
 
 //
 
 export function Mutate_Exchange({ categories }: { categories: Category[] }) {
   const create = TRPC_React.exchanges.create.useMutation();
   const utils = TRPC_React.useUtils();
+  const router = useRouter();
 
   return (
     <Formik
@@ -30,6 +32,8 @@ export function Mutate_Exchange({ categories }: { categories: Category[] }) {
         });
 
         await utils.exchanges.find.invalidate();
+
+        router.push("/exchanges");
       }}
     >
       {(formik) => <Exchange_CreateForm categories={categories} {...formik} />}
