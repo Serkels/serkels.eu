226ceb132c4d072415ac242837a43b004544fa71
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-13 02:13:48 +0100

    Revert "feat(exchange): use parallel routing"
    This reverts commit 776f415f8206adf68fcd1edc6618bca31c13ff0d.

--- apps/www/app/(main)/door/[code]/(private)/exchanges/inbox/[exchange_id]/layout.tsx
+++ apps/www/app/(main)/door/[code]/(private)/exchanges/inbox/[exchange_id]/layout.tsx
@@ -2,7 +2,8 @@
 
 import type { Params } from ":pipes/exchange_by_id";
 import type { Metadata, ResolvingMetadata } from "next";
-import type { PropsWithChildren, ReactNode } from "react";
+import type { PropsWithChildren } from "react";
+import Navbar_Page from "./_@navbar/page";
 
 //
 
@@ -18,15 +19,15 @@ export async function generateMetadata(
 //
 export default function Layout({
   children,
-  nav,
   params,
-}: PropsWithChildren<{ params: Params; nav: ReactNode }>) {
+}: PropsWithChildren<{ params: Params }>) {
   //! HACK(douglasduteil): Investigate way the param is "undefined" on direct page access
   if (params.exchange_id === "undefined") return null;
 
+  const navbar = <Navbar_Page params={params} />;
   return (
     <div className="grid h-full lg:grid-cols-[minmax(0,_300px),_1fr]">
-      <aside className="hidden pt-10 md:block">{nav}</aside>
+      <aside className="hidden pt-10 md:block">{navbar}</aside>
       <div className="bg-white">{children}</div>
     </div>
   );
