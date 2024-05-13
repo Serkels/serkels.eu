299bb3c3b26ae54234034adfa3d7692169227999
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-05-13 16:36:48 +0200

    feat(ui): add search icon in placeholder

--- packages/@1/ui/react/src/input/InputSearch.tsx
+++ packages/@1/ui/react/src/input/InputSearch.tsx
@@ -5,6 +5,7 @@ import {
   type ComponentPropsWithoutRef,
   type ElementRef,
 } from "react";
+import { Search } from "../icons";
 
 export const InputSearch = forwardRef<ElementRef<"input">, InputSearch_Props>(
   function InputSearch({ className, ...other_props }, forwardedRef) {
@@ -22,28 +23,14 @@ export const InputSearch = forwardRef<ElementRef<"input">, InputSearch_Props>(
 
         <div className="relative">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg
-              aria-hidden="true"
-              className="h-[45%] text-Dove_Gray"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-              />
-            </svg>
+            <Search className="h-[45%] w-6 text-Dove_Gray" />
           </div>
 
           <input
             ref={forwardedRef}
             type="search"
             id="default-search"
-            className="search-cancel:text-black block w-full rounded-full border border-[#B7B7B7] p-3 pl-9 text-xs outline-none"
+            className="search-cancel:text-black text-s block w-full rounded-full border border-[#B7B7B7] p-2 pl-12 outline-none"
             placeholder="Recherche"
             {...other_props}
           />
