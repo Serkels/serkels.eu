c8eaef92766f4c89454ceca6ce496bb92dcd2467
Douglas DUTEIL <douglasduteil@gmail.com>
2024-06-10 18:50:28 +0200

    feat: make it work somehow

--- apps/www/app/(main)/door/[code]/(private)/parameters/_client/Avatar_Editor.tsx
+++ apps/www/app/(main)/door/[code]/(private)/parameters/_client/Avatar_Editor.tsx
@@ -1,143 +1,89 @@
 "use client";
 
-import { TRPC_React } from ":trpc/client";
 import type { Profile } from "@1.modules/profile.domain";
 import { Avatar } from "@1.modules/profile.ui";
 import { Button } from "@1.ui/react/button";
-import { input, label } from "@1.ui/react/form/atom";
-import { Field, Form, Formik } from "formik";
+import { label } from "@1.ui/react/form/atom";
+import { useAsync } from "@react-hookz/web";
+import { fromImage } from "imtool";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import Preview, { Input } from "./Avatar_Preview";
+import { uploadImage } from "../upload";
 
 //
 
-// const Preview = dynamic(() => import("./Avatar_Preview"));
-interface FormInputs {
-  avatar: string;
-}
-
 export default function Avatar_Editor({ profile }: { profile: Profile }) {
-  const { register, handleSubmit, formState, getValues } = useForm<FormInputs>({
-    defaultValues: { avatar: profile.image },
+  const {
+    register,
+    formState: { isDirty, isSubmitting },
+    reset,
+  } = useForm({
+    defaultValues: { profile_id: profile.id, image_file: new File([], "") },
   });
+  const [preview, set_preview] = useState(profile.image);
   const { update } = useSession();
   const router = useRouter();
-  const update_image_to_gravatar =
-    TRPC_React.profile.me.update_image_to_gravatar.useMutation();
-  const { mutateAsync: save } = TRPC_React.profile.me.update.useMutation();
-  const onSubmit = useCallback(async (data: FormInputs) => {
-    await save({ image: data.avatar });
+  const [{ status }, { execute }] = useAsync(async (data: FormData) => {
+    const file = data.get("image_file") as File;
+    const tool = await fromImage(file);
+    const thumbnail = await tool
+      .thumbnail(258, true)
+      .type("image/jpeg")
+      .toFile(`@${profile.id}.jpg`);
+    data.set("image_file", thumbnail);
+    await uploadImage(data);
     await update();
-  }, []);
+    reset();
+    router.refresh();
+  });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <pre>
-        {JSON.stringify({ formState, isDirty: formState.isDirty }, null, 2)}
-      </pre>
-      <fieldset className="md:col-span-1">
+    <form action={execute}>
+      <fieldset
+        className="md:col-span-1"
+        disabled={isSubmitting || status === "loading"}
+      >
         <label className={label()} htmlFor="avatar">
           Avatar :
           <br />
           <small className="text-xs">
             <p>Changer l'url utiliser comme avatar.</p>
-            <p>
-              Faites glisser et déposez un fichiers ici, ou cliquez pour
-              sélectionner un fichiers
-            </p>
           </small>
         </label>
-      </fieldset>
-      <div className="flex flex-col justify-center md:col-span-1">
-        <Preview
-          profile={{
-            id: profile.id,
-            image: getValues("avatar"),
+        <input type="hidden" value={profile.id} {...register("profile_id")} />
+        <input
+          type="file"
+          {...register("image_file", {
+            async onChange(event: React.ChangeEvent<HTMLInputElement>) {
+              const file = event.target.files?.[0];
+              if (!file) return;
+
+              // if (file.size > 1e7) {
+              //   window.alert("Please upload a file smaller than 10 MB");
+              //   return false;
+              // }
+              set_preview(URL.createObjectURL(file));
+            },
+          })}
+          required
+        />
+        <Avatar
+          className="mx-auto size-48"
+          profile={{ id: profile.id, image: preview }}
+          onLoad={() => {
+            URL.revokeObjectURL(preview);
           }}
+        />
+        <Button
+          intent="primary"
+          type="submit"
+          isDisabled={!isDirty || isSubmitting || status === "loading"}
         >
-          <Input>
-            {(props) => <input {...register("avatar")} {...props.dropzone} />}
-          </Input>
-        </Preview>
-      </div>
-      <Button intent="primary" type="submit" isDisabled={!formState.isDirty}>
-        Sauvegarder
-      </Button>
+          Sauvegarder
+        </Button>
+      </fieldset>
     </form>
-  );
-  return (
-    <Formik
-      initialValues={{ url: profile.image }}
-      onSubmit={async ({ url }) => {
-        await save({ ...profile, image: url });
-        await update();
-        router.refresh();
-      }}
-    >
-      {({ dirty, values, setFieldValue }) => (
-        <Form className="grid grid-cols-2 gap-5">
-          <fieldset className="md:col-span-1">
-            <label className={label()} htmlFor="avatar">
-              Avatar :
-              <br />
-              <small className="text-xs">
-                Changer l'url utiliser comme avatar.
-              </small>
-            </label>
-
-            <Field className={input()} name="url" id="avatar" />
-
-            <details className="my-3">
-              <summary>
-                <Button
-                  intent="secondary"
-                  onPress={async () => {
-                    const new_profile =
-                      await update_image_to_gravatar.mutateAsync();
-                    await update();
-                    await setFieldValue("url", new_profile.image);
-                  }}
-                  type="button"
-                >
-                  Revenir à l'image Gravatar
-                </Button>
-              </summary>
-              <br />
-              Par default votes image profile est demender à{" "}
-              <a
-                className="underline"
-                target="_blank"
-                rel="noreferrer"
-                href="https://gravatar.com/"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                Gravatar
-              </a>
-            </details>
-
-            <Button intent="primary" type="submit" isDisabled={!dirty}>
-              Sauvegarder
-            </Button>
-          </fieldset>
-          <div className="flex flex-col justify-center md:col-span-1">
-            <label className={label()} htmlFor="avatar">
-              Preview :
-            </label>
-            <Avatar
-              className="mx-auto h-48"
-              profile={{
-                id: profile.id,
-                image: values.url,
-              }}
-            />
-          </div>
-        </Form>
-      )}
-    </Formik>
   );
 }
--- apps/www/app/(main)/door/[code]/(private)/parameters/_client/Avatar_Preview.tsx
+++ apps/www/app/(main)/door/[code]/(private)/parameters/_client/Avatar_Preview.tsx
@@ -1,13 +1,12 @@
-"use client";
+//
 
 import type { Profile } from "@1.modules/profile.domain";
 import { Avatar } from "@1.modules/profile.ui";
 import { label } from "@1.ui/react/form/atom";
 import { useUnmountEffect } from "@react-hookz/web";
 import { fromImage } from "imtool";
-import { useState, type PropsWithChildren } from "react";
-import { useDropzone, type DropzoneInputProps } from "react-dropzone";
-import { createSlot } from "react-slotify";
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
 import { tv } from "tailwind-variants";
 
 //
@@ -16,15 +15,14 @@ interface FileWithPreview extends File {
   preview: string;
 }
 
-export const Input = createSlot<{ dropzone: DropzoneInputProps }>();
 export default function Avatar_Previews({
-  children,
   profile,
-}: PropsWithChildren<{
+  set_profile_image,
+}: {
   profile: Pick<Profile, "image" | "id">;
-  // set_profile_image: (image: string) => void;
-}>) {
-  const [file, setFile] = useState({
+  set_profile_image: (file: File) => void;
+}) {
+  const [file, set_file] = useState({
     preview: profile.image,
   } as FileWithPreview);
   const { getRootProps, getInputProps } = useDropzone({
@@ -32,19 +30,23 @@ export default function Avatar_Previews({
       "image/*": [],
     },
     maxFiles: 1,
-    onDrop: async ([file]) => {
+    async onDrop([file]) {
       if (!file) return;
       const tool = await fromImage(file);
-      const thumbnail = await tool.thumbnail(258, true).toFile(profile.id);
+      const thumbnail = await tool
+        .thumbnail(258, true)
+        .type("image/jpeg")
+        .toFile(`@${profile.id}.jpeg`);
       const thumbnail_file: FileWithPreview = Object.assign(thumbnail, {
         preview: URL.createObjectURL(thumbnail),
       });
-      setFile(thumbnail_file);
-      // set_profile_image(thumbnail_file.preview);
+      set_file(thumbnail_file);
+      set_profile_image(file);
     },
   });
 
   useUnmountEffect(() => {
+    // from https://react-dropzone.js.org/#!/Previews
     // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
     URL.revokeObjectURL(file.preview);
   });
@@ -56,7 +58,7 @@ export default function Avatar_Previews({
         Aperçu :
       </label>
       <div {...getRootProps({ className: base() })}>
-        <Input.Renderer childs={children} dropzone={getInputProps()} />
+        <input {...getInputProps()} />
         <Avatar
           className="mx-auto size-48"
           profile={{
