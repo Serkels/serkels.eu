3e9a483bc66fc637176025f35b5bb343b99b461b
Johan Nilsson <95536608+JNSPK@users.noreply.github.com>
2024-06-11 16:16:31 +0200

    Update apps/www/app/(main)/forum/_client/SearchForm.tsx
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>

--- apps/www/app/(main)/forum/_client/SearchForm.tsx
+++ apps/www/app/(main)/forum/_client/SearchForm.tsx
@@ -1,19 +1,5 @@
 "use client";
 
-// import { useSyncSearchQuery } from ":components/hooks/useSyncSearchQuery";
-// import InputSearch from "@1.ui/react/input/InputSearch";
-// import { useDebouncedCallback } from "@react-hookz/web";
-// import type { ComponentProps } from "react";
-
-// //
-
-// export default function SearchForm() {
-//   const { query, setQuery } = useSyncSearchQuery("q");
-//   const onChange = useDebouncedCallback<
-//     NonNullable<ComponentProps<"input">["onChange"]>
-//   >((ev) => setQuery(ev.target.value), [setQuery], 1_666, 500);
-//   return <InputSearch onChange={onChange} defaultValue={query} />;
-// }
 
 "use client";
 
