af990212cb48e9f0ada491e0c98215d692bd9557
Johan Nilsson <95536608+JNSPK@users.noreply.github.com>
2024-06-11 17:29:20 +0200

    fix(responsive): flex-row into info bar (#1390)
    fix(responsive):flex-row into info bar

--- packages/@1/modules/exchange/ui/src/Card/exchange_card.tsx
+++ packages/@1/modules/exchange/ui/src/Card/exchange_card.tsx
@@ -11,7 +11,8 @@ export const exchange_card = tv({
     header: "grid grid-cols-3 items-center",
     exchange_icon: "mx-1 w-5",
     footer: "mt-4 rounded-b-xl bg-gray-500 px-5 py-3 text-white",
-    info_bar: "flex flex-col justify-between gap-y-2 text-xs text-[#707070]",
+    info_bar:
+      "flex flex-col justify-between gap-y-2 text-xs text-[#707070] md:flex-row",
   },
   variants: {
     type: {
