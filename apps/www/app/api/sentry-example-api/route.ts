fbceb133b674fa4d7ba97fa532725bcc925dede7
Douglas DUTEIL <douglasduteil@gmail.com>
2024-06-11 11:33:09 +0200

    test error on sentry

--- apps/www/app/api/sentry-example-api/route.ts
+++ apps/www/app/api/sentry-example-api/route.ts
@@ -5,7 +5,9 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 // A faulty API route to test Sentry's error monitoring
-export function GET() {
-  throw new Error("Sentry Example API Route Error");
+export function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  if (id) throw new Error(`Sentry Example API Route Error ${id}`);
   return NextResponse.json({ data: "Testing Sentry Error..." });
 }
