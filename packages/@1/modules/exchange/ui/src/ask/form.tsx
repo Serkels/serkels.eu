f1fae4641a962c8a4f7b811f06f8882250ba51fe
Douglas Duteil <douglasduteil@gmail.com>
2024-06-12 17:31:40 +0200

    Update packages/@1/modules/exchange/ui/src/ask/form.tsx
    Co-authored-by: Johan Nilsson <95536608+JNSPK@users.noreply.github.com>

--- packages/@1/modules/exchange/ui/src/ask/form.tsx
+++ packages/@1/modules/exchange/ui/src/ask/form.tsx
@@ -91,7 +91,7 @@ function use_ask_form() {
     )
     .with(
       { return_id: P.nullish, type: Exchange_TypeSchema.Enum.RESEARCH },
-      () => "Bonjour, je veux bien répondre a votre recherche.",
+      () => "Bonjour, je veux bien répondre à votre recherche.",
     )
     .otherwise(() => "Bonjour, je veux bien échanger avec toi.");
 
