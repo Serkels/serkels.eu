c77624660c0dea66ba76f7a15d997c79439d024b
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-06-14 14:47:36 +0200

    fix(responsive): button display only on mobile

--- apps/www/components/button/BackButton.tsx
+++ apps/www/components/button/BackButton.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function BackButton({ href }: { href: string }) {
   return (
-    <Link href={href}>
+    <Link className="md:hidden" href={href}>
       <Image src="/back.svg" alt="back" width={20} height={20} priority />
     </Link>
   );
