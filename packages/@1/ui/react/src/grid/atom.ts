8b8e277acd90f303148ac17158ebfbc8ac84ba8f
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-06-19 19:18:51 +0200

    fix(ui): improvement for mobile responsiveness

--- packages/@1/ui/react/src/grid/atom.ts
+++ packages/@1/ui/react/src/grid/atom.ts
@@ -6,6 +6,8 @@ import { tv } from "tailwind-variants";
 
 export const column_screen = tv({
   base: `
+    max-h-[calc(100vh_-_theme(spacing.24
+    ))]
     flex
     h-full
     max-h-[calc(100vh_-_theme(spacing.32))]
