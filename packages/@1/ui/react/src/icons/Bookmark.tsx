a2951f56480ddeb62c265eee8546ad1dea33b620
Douglas DUTEIL <douglasduteil@gmail.com>
2024-08-26 00:22:23 +0200

    fix(exchange): share bookmark size

--- packages/@1/ui/react/src/icons/Bookmark.tsx
+++ packages/@1/ui/react/src/icons/Bookmark.tsx
@@ -2,6 +2,8 @@
 
 import type { SVGProps } from "react";
 
+//
+
 export function Bookmark(props: SVGProps<any>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
