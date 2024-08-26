09f29d03b051f3b97fe22c7fbf0f0e3580f227d6
Douglas Duteil <douglasduteil@gmail.com>
2024-08-26 18:00:50 +0200

    Update packages/@1/ui/react/src/icons/Messenger.tsx

--- packages/@1/ui/react/src/icons/Messenger.tsx
+++ packages/@1/ui/react/src/icons/Messenger.tsx
@@ -12,7 +12,6 @@ export function Messenger(props: SVGProps<any>) {
       {...props}
     >
       <path
-        id="messenger"
         d="M0,8.444a8.21,8.21,0,0,0,3.4,6.575l-.99,4.447,4.1-2.931a9.818,9.818,0,0,0,2.616.353c5.036,0,9.12-3.78,9.12-8.444S14.156,0,9.12,0,0,3.78,0,8.444Z"
         fill="currentColor"
       />
