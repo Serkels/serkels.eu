b36fb466952b8e7a8180e48dbdd4d504f70cccef
Johan Nilsson <95536608+JNSPK@users.noreply.github.com>
2024-08-30 18:29:09 +0200

    feat(inbox): change inbox icon (#1737)
    * feat(inbox): change inbox icon
    
    
    ---------
    
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>

--- packages/@1/ui/react/src/icons/Messenger.tsx
+++ packages/@1/ui/react/src/icons/Messenger.tsx
@@ -4,10 +4,15 @@ import type { SVGProps } from "react";
 
 export function Messenger(props: SVGProps<any>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18.239"
+      height="19.467"
+      viewBox="0 0 18.239 19.467"
+      {...props}
+    >
       <path
-        className="fill-current"
-        d="M12 0C5.373 0 0 4.974 0 11.111a10.8 10.8 0 0 0 4.438 8.633l.031.021V24l4.088-2.242c1.031.295 2.215.464 3.439.464H12c6.627 0 12-4.975 12-11.11S18.627 0 12 0zm1.191 14.963L10.136 11.7l-5.963 3.26L10.732 8l3.131 3.259L19.752 8z"
+        d="M0,8.444a8.21,8.21,0,0,0,3.4,6.575l-.99,4.447,4.1-2.931a9.818,9.818,0,0,0,2.616.353c5.036,0,9.12-3.78,9.12-8.444S14.156,0,9.12,0,0,3.78,0,8.444Z"
         fill="currentColor"
       />
     </svg>
