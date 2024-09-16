baf98254c1c197957c6ecc494b11636d6eb60b97
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-16 16:33:11 +0200

    fix(www): mobile menu burger

--- apps/www/app/(home)/layout.tsx
+++ apps/www/app/(home)/layout.tsx
@@ -41,7 +41,7 @@ function AppLargeTopBar() {
       <Link href="/">
         <SerkelsLogo />
       </Link>
-      <MenuBurger className="right-9 top-6" />
+      <MenuBurger className="md:right-9 md:top-6" />
     </BigBar>
   );
 }
