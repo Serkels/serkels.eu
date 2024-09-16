92b3d71b162801073a1954be43b7b0906389d0cd
Douglas Duteil <douglasduteil@gmail.com>
2024-09-16 11:55:00 +0200

    feat(www): update main logo (#1803)

--- apps/www/components/placeholder/Serkels_Placeholder.tsx
+++ apps/www/components/placeholder/Serkels_Placeholder.tsx
@@ -1,20 +1,13 @@
 //
 
-import Image from "next/image";
+import SerkelsLogo from ":components/shell/SerkelsLogo";
 
 //
 
 export function Serkels_Placeholder() {
   return (
     <figure className="flex h-full items-center justify-center">
-      <Image
-        className=""
-        src="/serkels-noir.svg"
-        alt="Serkels Logo"
-        width={175}
-        height={53}
-        priority
-      />
+      <SerkelsLogo />
       <figcaption></figcaption>
     </figure>
   );
