8264533b919a0d917283f2379f9dc771c4ae9f8f
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-18 01:11:35 +0200

    feat(www): debug tracking use @next/third-parties

--- apps/www/components/shell/Analytics.tsx
+++ apps/www/components/shell/Analytics.tsx
@@ -1,6 +1,7 @@
 //
 
 import { getServerSession } from "@1.modules/auth.next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Script from "next/script";
 import { Suspense } from "react";
@@ -8,7 +9,7 @@ import { Analytics_Client } from "./Analytics.client";
 
 //
 
-export const GA_TRACKING_ID = process.env["NEXT_PUBLIC_GA_MEASUREMENT_ID"];
+export const GA_TRACKING_ID = process.env["NEXT_PUBLIC_GA_MEASUREMENT_ID"]!;
 
 export default async function Analytics() {
   const session = await getServerSession();
@@ -29,29 +30,7 @@ function gtag(){dataLayer.push(arguments);}
     <>
       <SpeedInsights />
 
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        dangerouslySetInnerHTML={{
-          __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-
-gtag('js', new Date());
-
-gtag('config', '${GA_TRACKING_ID}', {
-    page_path: window.location.pathname,
-    // transport_url: window.location.origin + '/api/stalker',
-    first_party_collection: true,
-    user_id: '${session?.profile.id ?? ""}',
-});
-`,
-        }}
-      />
-
+      <GoogleAnalytics gaId={GA_TRACKING_ID} />
       <Suspense>
         <Analytics_Client />
       </Suspense>
