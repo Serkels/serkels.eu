358db00c6a6899b43795ff8decec3fff9f507612
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-09-17 20:00:03 +0200

    fix(exchange): Exchange editing issue

--- apps/www/app/(main)/door/[code]/(public)/proposals/page.tsx
+++ apps/www/app/(main)/door/[code]/(public)/proposals/page.tsx
@@ -1,8 +1,10 @@
 //
 
+import { NotConnected_Placeholder } from ":components/placeholder/NotConnected_Placeholder";
 import { code_to_profile_id, type CodeParms } from ":pipes/code";
 import { TRPC_Hydrate, TRPC_SSR } from ":trpc/server";
 import { Exchange_Card } from ":widgets/exchanges/card";
+import { getServerSession } from "@1.modules/auth.next";
 import { PROFILE_ROLES } from "@1.modules/profile.domain";
 import type { Metadata, ResolvingMetadata } from "next";
 import { notFound, redirect } from "next/navigation";
@@ -33,6 +35,12 @@ export default async function Page({ params }: { params: CodeParms }) {
     profile_id,
   });
 
+  const session = await getServerSession();
+
+  if (!session) {
+    return <NotConnected_Placeholder />;
+  }
+
   return (
     <TRPC_Hydrate>
       <main className="grid grid-cols-1 gap-y-5">
@@ -40,7 +48,7 @@ export default async function Page({ params }: { params: CodeParms }) {
           <Exchange_Card
             key={exchange.id}
             exchange={exchange}
-            profile={profile}
+            profile={session.profile}
           />
         ))}
       </main>
