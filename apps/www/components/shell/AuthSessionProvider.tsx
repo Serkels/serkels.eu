e897b105bbf3cc657827360ddcda678bc47de1e9
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-18 02:11:21 +0200

    feat(www): track the user

--- apps/www/components/shell/AuthSessionProvider.tsx
+++ apps/www/components/shell/AuthSessionProvider.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { sendGAEvent } from "@next/third-parties/google";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import type { PropsWithChildren } from "react";
+import { useEffect, type PropsWithChildren } from "react";
 
 export function AuthSessionProvider({
   children,
   session,
 }: PropsWithChildren<{ session: Session | null }>) {
+  useEffect(() => {
+    const user_id = session?.profile.id;
+    if (!user_id) return;
+    sendGAEvent("set", { user_id });
+  }, [session?.profile.id]);
   return <SessionProvider session={session}> {children} </SessionProvider>;
 }
