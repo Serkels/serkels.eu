8af98ebdc099a57f0fdaee21c46d0dcc0ee7609d
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-30 15:27:32 +0200

    deploy(vercel): use VERCEL_URL on emails (7)

--- packages/@1/modules/core/src/env.zod.ts
+++ packages/@1/modules/core/src/env.zod.ts
@@ -7,8 +7,27 @@ import { z } from "zod";
 export const env_app_url_schema = z
   .object({
     APP_URL: z.string().url().default("http://localhost:3000"),
+    NEXT_PUBLIC_VERCEL_ENV: z.string().optional(),
+    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z.string().url().optional(),
+    NEXT_PUBLIC_VERCEL_URL: z.string().url().optional(),
     VERCEL_URL: z.string().url().optional(),
   })
-  .transform(({ APP_URL, VERCEL_URL }) => ({
-    APP_URL: VERCEL_URL ?? APP_URL,
-  }));
+  .transform(
+    ({
+      APP_URL,
+      NEXT_PUBLIC_VERCEL_ENV,
+      NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+      NEXT_PUBLIC_VERCEL_URL,
+      VERCEL_URL,
+    }) => ({
+      APP_URL:
+        (NEXT_PUBLIC_VERCEL_ENV === "production"
+          ? `https://${NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+          : undefined) ??
+        (NEXT_PUBLIC_VERCEL_ENV === "preview"
+          ? `https://${NEXT_PUBLIC_VERCEL_URL}`
+          : undefined) ??
+        VERCEL_URL ??
+        APP_URL,
+    }),
+  );
