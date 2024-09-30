3fc934255afb391b4b1aecfc35821e07cec079ef
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-30 16:08:15 +0200

    deploy(vercel): use VERCEL_URL on emails (8)

--- packages/@1/modules/core/src/env.zod.ts
+++ packages/@1/modules/core/src/env.zod.ts
@@ -8,9 +8,21 @@ export const env_app_url_schema = z
   .object({
     APP_URL: z.string().url().default("http://localhost:3000"),
     NEXT_PUBLIC_VERCEL_ENV: z.string().optional(),
-    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z.string().url().optional(),
-    NEXT_PUBLIC_VERCEL_URL: z.string().url().optional(),
-    VERCEL_URL: z.string().url().optional(),
+    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z
+      .string()
+      .transform((hostname) => `https://${hostname}`)
+      .pipe(z.string().url())
+      .optional(),
+    NEXT_PUBLIC_VERCEL_URL: z
+      .string()
+      .transform((hostname) => `https://${hostname}`)
+      .pipe(z.string().url())
+      .optional(),
+    VERCEL_URL: z
+      .string()
+      .transform((hostname) => `https://${hostname}`)
+      .pipe(z.string().url())
+      .optional(),
   })
   .transform(
     ({
@@ -22,10 +34,10 @@ export const env_app_url_schema = z
     }) => ({
       APP_URL:
         (NEXT_PUBLIC_VERCEL_ENV === "production"
-          ? `https://${NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+          ? NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
           : undefined) ??
         (NEXT_PUBLIC_VERCEL_ENV === "preview"
-          ? `https://${NEXT_PUBLIC_VERCEL_URL}`
+          ? NEXT_PUBLIC_VERCEL_URL
           : undefined) ??
         VERCEL_URL ??
         APP_URL,
