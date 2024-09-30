fdd560976bfd0d947e66218db256dad02c76964a
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-30 14:43:50 +0200

    deploy(vercel): use VERCEL_URL on emails (4)

---
+++ packages/@1/modules/core/src/env.zod.ts
@@ -0,0 +1,12 @@
+//
+
+import { z } from "zod";
+
+//
+
+export const env_app_url_schema = z.object({
+  APP_URL: z.string().url().default("http://localhost:3000"),
+  VERCEL_URL: z.string().optional(),
+}).transform(({ APP_URL, VERCEL_URL }) => ({
+  APP_URL: VERCEL_URL ?? APP_URL,
+}))
