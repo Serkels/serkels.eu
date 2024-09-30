af8dd8e1f9fc518a0d404b7785254e9b68a1d533
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-30 12:18:20 +0200

    chore: clean up (5)

--- packages/@1/modules/auth/next/src/index.ts
+++ packages/@1/modules/auth/next/src/index.ts
@@ -4,8 +4,6 @@ import { auth } from "./auth.config";
 import "./next-auth.d.ts";
 export * from "./auth.config";
 
-export { get_csrf_token } from "./csrf_token";
-
 /**
  * @deprecated Use `authimport type adapter from "@douglasduteil/nextauth...trpc.prisma/trpc/router/adapter";
 ` instead
