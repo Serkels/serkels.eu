71771b4a9dacca333f088d253b0b8e88db980f01
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-11-02 14:55:40 +0100

    feat(profile): add some color to the counts

--- apps/www/app/(main)/door/[code]/(public)/@header/CircleCount.tsx
+++ apps/www/app/(main)/door/[code]/(public)/@header/CircleCount.tsx
@@ -9,8 +9,8 @@ export function CircleCount({ profile_id }: { profile_id: string }) {
   const count = profile.data?.contacts.length ?? 0;
   return (
     <div aria-label="Mes cercles" className="flex flex-col items-center">
-      <div className="text-lg font-bold">{count}</div>
-      <div>Cercle{count > 1 ? "s" : ""}</div>
+      <div className="text-lg font-bold text-primary">{count}</div>
+      <div className="text-primary">Cercle{count > 1 ? "s" : ""}</div>
     </div>
   );
 }
