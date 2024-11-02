3ee72ae58bdda6120b135759e8ef70066c13d29b
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-11-02 13:42:14 +0100

    feat(footer): Move burger menu into footer

--- apps/www/components/shell/AppFooter.server.tsx
+++ apps/www/components/shell/AppFooter.server.tsx
@@ -1,7 +1,8 @@
 //
 
+import { format, parseISO } from "date-fns";
+import { fr } from "date-fns/locale";
 import { Footer } from "./Footer";
-
 export async function AppFooter() {
   const now = await getServerDate();
   const year = new Date(now).getFullYear();
@@ -9,5 +10,13 @@ export async function AppFooter() {
 }
 
 async function getServerDate() {
-  return new Date().toISOString();
+  const dateString = new Date().toISOString();
+
+  const day = format(parseISO(dateString), "dd", { locale: fr });
+  const month = format(parseISO(dateString), "MMMM", { locale: fr });
+  const year = format(parseISO(dateString), "yyyy", { locale: fr });
+
+  const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
+
+  return `${day} ${capitalizedMonth} ${year}`;
 }
