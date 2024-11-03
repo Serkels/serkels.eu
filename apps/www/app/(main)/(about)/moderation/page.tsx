0219672e394f036f7ab7334e4828ff85bb28e8c0
Johan Nilsson <95536608+JNSPK@users.noreply.github.com>
2024-11-03 19:47:21 +0100

    feat(moderation):add moderation page from commit 3ee72ae (#2002)
    * Ajout du fichier de moderation depuis le commit 3ee72ae
    
    * feat(moderation):refactoring to put info into content.md

---
+++ apps/www/app/(main)/(about)/moderation/page.tsx
@@ -0,0 +1,37 @@
+//
+
+import { readFile } from "fs/promises";
+import type { Metadata, ResolvingMetadata } from "next";
+import dynamic from "next/dynamic";
+import { join } from "path";
+
+//
+
+const ReactMarkdown = dynamic<any>(() => import("react-markdown"));
+
+const content = await readFile(
+  join(process.cwd(), "app/(main)/(about)/moderation/content.md"),
+  "utf8",
+);
+
+export async function generateMetadata(
+  _: never,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const title = `Moderation Rules :: ${(await parent).title?.absolute}`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+    },
+  };
+}
+
+export default function Page() {
+  return (
+    <main className="container prose mx-auto my-10 p-6 lg:prose-xl">
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </main>
+  );
+}
