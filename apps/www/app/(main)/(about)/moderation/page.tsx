f8ca36919cfb79a773e22a609e6c3f945612fdfd
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-11-03 18:31:10 +0100

    feat(moderation):refactoring to put info into content.md

--- apps/www/app/(main)/(about)/moderation/page.tsx
+++ apps/www/app/(main)/(about)/moderation/page.tsx
@@ -1,105 +1,32 @@
 //
 
+import { readFile } from "fs/promises";
+import type { Metadata, ResolvingMetadata } from "next";
 import dynamic from "next/dynamic";
+import { join } from "path";
 
 //
 
 const ReactMarkdown = dynamic<any>(() => import("react-markdown"));
 
-const content = `
-# Règles de modération SERKELS
+const content = await readFile(
+  join(process.cwd(), "app/(main)/(about)/moderation/content.md"),
+  "utf8",
+);
 
-Les présentes Règles de modération sont destinées aux utilisateurs de la plateforme SERKELS (ci-après « SERKELS »), exploitée par l’Union des étudiants exilés, association Loi 1901 dont le siège est situé 142 rue de Rivoli, 75001 Paris (ci-après « l’UEE »).  
+export async function generateMetadata(
+  _: never,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const title = `Moderation Rules :: ${(await parent).title?.absolute}`;
 
-En utilisant SERKELS, les utilisateurs acceptent de se soumettre aux Règles de modération.  
-
-Ces règles décrivent le comportement attendu des utilisateurs de SERKELS, ainsi que les principes de modération appliqués par les équipes de l’UEE.
-
-## Comportement des utilisateurs
-
-Les utilisateurs de SERKELS s’engagent à se comporter à l’égard des autres utilisateurs et du personnel de l’UEE de manière courtoise, en respectant les règles de la politesse et dans une attitude respectueuse de tous.  
-
-Les utilisateurs de SERKELS s’interdisent notamment :
-
-- Tout comportement agressif ou violent, ainsi que toute menace ;
-- Tout propos pouvant être constitutif de diffamation, d’injure, d’incitation à la haine ou de toute autre infraction pénalement sanctionnée ;
-- Tout comportement constitutif de harcèlement ;
-- Toute forme de harcèlement sexuel et tout comportement visant à contraindre un autre utilisateur à adopter un comportement ou réaliser un acte de nature romantique ou sexuel ;
-- Tout comportement illicite de quelque nature et sous quelque forme que ce soit.
-
-Les utilisateurs s’engagent par ailleurs à utiliser SERKELS conformément aux Conditions Générales d’Utilisation, et en particulier s’interdisent d’utiliser SERKELS pour des finalités étrangères à leurs études.  
-
-Il est rappelé à l’utilisateur que SERKELS n’est pas responsable des actions et comportements de ses utilisateurs qui se déroulent en-dehors de la plateforme SERKELS. Toutefois, si un comportement illicite avéré d’un utilisateur de SERKELS est signalé à l’UEE, ayant été préparé à l’aide de SERKELS ou facilité par l’utilisation de SERKELS, l’UEE se réserve le droit de retirer à cet utilisateur son accès à SERKELS.
-
-## Principes de modération appliqués par l’UEE
-
-### Outils mis à disposition des utilisateurs
-
-#### Signalements de contenus
-
-Les utilisateurs de SERKELS peuvent signaler des contenus contrevenant aux présentes Règles de modération ou à toute loi ou réglementation applicable en utilisant la fonctionnalité « signaler », qui se manifeste par une icône drapeau située dans le coin de tous les contenus publiés sur SERKELS.  
-
-Un utilisateur peut ainsi signaler une annonce, un message… Lorsque l’utilisateur souhaite signaler un contenu et clique sur l’icône « signaler », il accède à un formulaire lui permettant de renseigner avec précision les faits qu’il souhaite signaler.
-
-L’utilisateur est invité à fournir autant de précisions que possible, ainsi que tout élément justificatif permettant d’étayer le signalement (captures écran, liens hypertexte…).
-
-#### Blocage entre utilisateurs
-
-Les utilisateurs de SERKELS peuvent bloquer tout autre utilisateur à partir de l’outil de messagerie de SERKELS. Lorsqu’un utilisateur « bloque » un autre utilisateur, il signale à SERKELS qu’il ne souhaite plus recevoir de messages de la part de ce dernier.
-
-L’utilisateur bloqué ne pourra plus entrer en contact avec l’utilisateur qui a utilisé la fonctionnalité de blocage à son encontre.  
-
-Un utilisateur peut « bloquer » ou retirer le blocage à l’encontre d’un autre utilisateur librement, à tout moment et autant de fois qu’il le souhaite.
-
-### Mesures de sanction appliquées par l’UEE
-
-Lorsque l’UEE a connaissance de faits contrevenant aux présentes Règles de modération, soit à la suite d’un signalement, soit par tout autre moyen, l’UEE peut sanctionner tout utilisateur à l’origine de ces faits.
-
-#### Suspension du compte utilisateur
-
-L’UEE peut suspendre le compte d’un utilisateur de manière temporaire. La suspension du compte peut survenir pour plusieurs motifs :
-
-- L’UEE mène une enquête sur l’utilisateur suite à un signalement. La suspension du compte prend fin lorsque l’UEE conclut son enquête.
-- L’UEE attend la survenance d’un évènement (décision d’une autorité ou d’une juridiction…) avant de prendre une décision définitive relative à la suppression ou non du compte.
-- L’UEE estime que les faits contrevenant les Règles de modération sont suffisamment graves pour justifier une sanction mais ne permettent pas de justifier la suppression du compte. La durée de la suspension dépend alors de la gravité des actes de l’utilisateur justifiant la suspension et les motifs de la suspension.
-
-#### Suppression du compte utilisateur
-
-En cas de manquement suffisamment grave aux présentes Règles de modération, rendant impossible le maintien de l’utilisateur sur la plateforme SERKELS, l’UEE se réserve le droit de supprimer le compte de l’utilisateur sur SERKELS.
-
-L’utilisateur est alors placé sur une « liste rouge » et ne pourra pas ouvrir de nouveau compte sur SERKELS.  
-
-**Toute suppression de compte est définitive.**
-
-### Procédure de sanction et recours
-
-Lorsqu’un signalement visant un utilisateur est adressé à l’UEE, l’UEE informe cet utilisateur par courriel du signalement et des faits qui lui sont reprochés. L’utilisateur peut alors répondre à l’UEE par courriel, notamment pour apporter tout élément nécessaire à la parfaite compréhension des faits par l’UEE ou pour présenter sa défense.  
-
-L’équipe de modération de l’UEE étudie alors l’ensemble des éléments à sa disposition.
-
-L’UEE se réserve le droit de mener toute enquête sur SERKELS, et d’y collecter tout élément, qu’elle jugera pertinent aux fins de prendre une décision relative aux signalements qu’elle reçoit.  
-
-L’UEE informe l’utilisateur visé par un signalement par courriel de sa décision finale, et des motifs la justifiant : soit le signalement est écarté, soit l’UEE prend une mesure de sanction à l’égard de l’utilisateur.  
-
-En cas de sanction, l’utilisateur peut, dans un délai d’un mois à compter de la réception du courriel l’informant de la sanction, demander que la décision le concernant soit réexaminée.
-
-Il doit toutefois présenter des éléments de nature à démontrer que la décision a été prise en l’absence d’une information déterminante. Le cas échéant, le signalement est réexaminé par une formation interne de l’UEE composée de personnes n’ayant pas pris la décision initiale contestée.  
-
-La décision de la formation de réexamen est définitive.
-
-L’utilisateur en est informé par courriel.
-
----
-
-L’UEE peut modifier les Règles de modération à tout moment.
-
-Vous serez alors informé des modifications apportées aux Règles de modération.
-
-Si vous continuez à utiliser SERKELS, vous serez réputé avoir accepté les Règles de modération dans leur dernière version.
-
-**Dernière modification : 02/11/2024**
-
-`;
+  return {
+    title,
+    openGraph: {
+      title,
+    },
+  };
+}
 
 export default function Page() {
   return (
